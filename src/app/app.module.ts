10cb73e857495fa50e3116d6698e4927f7e2dc7e
Marcello <hey@marcello.codes>
2023-10-18 19:26:02 -0400

    Login error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthenticationInterceptor } from './interceptors/authentication.interce
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
@@ -31,6 +32,7 @@ import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
+    MatSnackBarModule,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
--- src/app/interceptors/authentication.interceptor.ts
+++ src/app/interceptors/authentication.interceptor.ts
@@ -9,10 +9,15 @@ import {
 import { Observable, catchError, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
-  constructor(private router: Router, private auth: AuthenticationService) {}
+  constructor(
+    private router: Router,
+    private auth: AuthenticationService,
+    private snackbar: MatSnackBar
+  ) {}
 
   intercept(
     request: HttpRequest<unknown>,
@@ -28,6 +33,10 @@ export class AuthenticationInterceptor implements HttpInterceptor {
         if (error.status === 401) {
           this.auth.logout();
           this.router.navigate(['/login']);
+          this.snackbar.open(
+            'You need to log in again to do this action',
+            'Dismiss'
+          );
         }
 
         return throwError(() => error);
