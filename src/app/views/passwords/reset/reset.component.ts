ae6508bc5ef91a10f9ba72e36d1e8ca25563f6e7
Marcello <hey@marcello.codes>
2023-10-22 13:40:36 -0400

    Password Reset

--- src/app/views/passwords/reset/reset.component.ts
+++ src/app/views/passwords/reset/reset.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
 import { take } from 'rxjs';
 import { PasswordService } from 'src/app/services/password.service';
 
@@ -8,27 +9,50 @@ import { PasswordService } from 'src/app/services/password.service';
   templateUrl: './reset.component.html',
   styleUrls: ['./reset.component.scss'],
 })
-export class ResetComponent {
+export class ResetComponent implements OnInit {
+  token = '';
   form = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
   });
 
   constructor(
     private snackbar: MatSnackBar,
-    private password: PasswordService
+    private password: PasswordService,
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      this.token = params.get('token') ?? '';
+    });
+  }
+
   submitForm(): void {
-    this.password
-      .forgot(this.form.get('email')?.value!)
-      .pipe(take(1))
-      .subscribe({
-        next: (response: any) => {
-          this.snackbar.open(response.message, 'Dismiss');
-        },
-        error: (error) => {
-          this.snackbar.open(error, 'Dismiss');
-        },
-      });
+    if (
+      this.form.get('password')!.value ===
+      this.form.get('confirmPassword')!.value
+    ) {
+      const request = {
+        password: this.form.get('password')!.value ?? '',
+        token: this.token,
+      };
+
+      this.password
+        .reset(request)
+        .pipe(take(1))
+        .subscribe({
+          next: (response: any) => {
+            this.snackbar.open(response.message, 'Dismiss');
+            this.router.navigate(['login']);
+          },
+          error: (error) => {
+            this.snackbar.open(error.error.message, 'Dismiss');
+          },
+        });
+    } else {
+      this.snackbar.open('Passwords do not match', 'Dismiss');
+    }
   }
 }
